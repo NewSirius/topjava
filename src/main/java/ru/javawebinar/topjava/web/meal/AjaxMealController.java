218eb84ffbbfd63decfa4fa0ec02641111327ce1
NewSirius <newsirius@mail.ru>
2018-05-02 10:18:53 +0300

    HW8 BASE

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/AjaxMealController.java
@@ -0,0 +1,59 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealWithExceed;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = AjaxMealController.AJAX_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class AjaxMealController extends AbstractMealController {
+
+    static final String AJAX_URL = "/ajax/profile/meals";
+
+    @Override
+    @GetMapping("/{id}")
+    public Meal get(@PathVariable("id") int id) {
+        return super.get(id);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @Override
+    @GetMapping
+    public List<MealWithExceed> getAll() {
+        return super.getAll();
+    }
+
+
+    @PostMapping
+    public void create(@RequestParam("id") Integer id,
+                       @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime localDateTime,
+                       @RequestParam("description") String description,
+                       @RequestParam("calories") int calories) {
+        Meal meal = new Meal(id, localDateTime, description, calories);
+        if (meal.isNew()) {
+            super.create(meal);
+        }
+    }
+
+    @Override
+    @GetMapping(value = "/filter")
+    public List<MealWithExceed> getBetween(
+            @RequestParam(value = "startDate", required = false) LocalDate startDate,
+            @RequestParam(value = "startTime", required = false) LocalTime startTime,
+            @RequestParam(value = "endDate", required = false) LocalDate endDate,
+            @RequestParam(value = "endTime", required = false) LocalTime endTime) {
+        return super.getBetween(startDate, startTime, endDate, endTime);
+    }
+}
